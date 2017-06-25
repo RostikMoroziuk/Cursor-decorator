111a2f05c668e103d8a0b75da06eb06da9e9bace
RostikMoroziuk <rmorozyukua@gmail.com>
2017-06-25 14:04:10 +0300

    Add properties for User class

--- js/User.js
+++ js/User.js
@@ -1,12 +1,29 @@
 //class
 function User(name, surname, age) {
+  //Discounts
+  var DISCOUNTS = {
+    //discount in percent
+    globalDiscount: 1,
+    nightDiscount: 2,
+    weekendDiscount: 2
+  }
   //user data
   this._name = name;
   this._surname = surname;
   this._age = age;
 
-  //bonus and discount info
-  this._lastVisitDate 
+  //bonus info
+  this._lastVisitDate = new Date();
+  this._bonus = parseInt(Math.random() * 10);
+
+  //and discount info
+  this.getDiscount = function() {
+    return DISCOUNTS;
+  }
+
+  //orders
+  this._ordersCount = parseInt(Math.random() * 10);
+  this._ordersTotalPrice = parseInt(Math.random() * 100);
 }
 
 User.prototype.getName = function () {
@@ -19,4 +36,20 @@ User.prototype.getSurname = function () {
 
 User.prototype.getAge = function () {
   return this._age;
+}
+
+User.prototype.getLastVisitDate = function () {
+  return this._lastVisitDate;
+}
+
+User.prototype.getBonus = function () {
+  return this._bonus;
+}
+
+User.prototype.getOrdersCount = function () {
+  return this._ordersCount;
+}
+
+User.prototype.getOrdersTotalPrice = function () {
+  return this._ordersTotalPrice;
 }
